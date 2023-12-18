e7d25ff45bddcdfff379eea355f0d45ddf9df4a2
ZoontS <89505339+ZoontS@users.noreply.github.com>
2023-12-18 23:07:56 +0700

    Update script.jl

--- script.jl
+++ script.jl
@@ -69,11 +69,11 @@ function calculate_accuracy(comparison)
     return accuracy
 end
 
-function filter_correct_prediction(data, comparison)
+function filter_correct_prediction(data, comparison, accuracies)
     correct_size = 0
     incorrect_size = 0
     for row in axes(data, 1)
-        if comparison[row] == true
+        if comparison[row, argmax(accuracies)] == true
             correct_size += 1
         else 
             incorrect_size += 1
@@ -82,10 +82,11 @@ function filter_correct_prediction(data, comparison)
 
     correct_data = Matrix{Float16}(undef, correct_size, 5)
     incorrect_data = Matrix{Float16}(undef, incorrect_size, 5)
+    incorrect_comparison = zeros(Bool, incorrect_size, 4)
     i = 1
     j = 1
     for row in axes(data, 1)
-        if comparison[row] == true
+        if comparison[row, argmax(accuracies)] == true
             for col in 1:5
                 correct_data[i, col] = data[row, col]
             end
@@ -93,12 +94,13 @@ function filter_correct_prediction(data, comparison)
         else
             for col in 1:5
                 incorrect_data[j, col] = data[row, col]
+                incorrect_comparison[j, :] = comparison[row, :]
             end
             j += 1
         end
     end
 
-    return correct_data, incorrect_data
+    return correct_data, incorrect_data, incorrect_comparison
 end
 
 function correct_prediction_sumless_mean(data, col)
@@ -125,22 +127,22 @@ function calculate_correct_prediction_averages(data, comparison, accuracies)
     averages = Matrix{Float16}(undef, 3, 4)
     temp_accuracies = copy(accuracies)
 
-    x1, remainder_data = filter_correct_prediction(data, comparison[:, argmax(temp_accuracies)])
+    x1, remainder_data, remainder_comparison = filter_correct_prediction(data, comparison, temp_accuracies)
     x1_averages = correct_prediction_sumless_mean(x1, argmax(temp_accuracies))
     averages[:, argmax(temp_accuracies)] = x1_averages
     temp_accuracies[argmax(temp_accuracies)] = 0.0
 
-    x2, remainder_data = filter_correct_prediction(remainder_data, comparison[:, argmax(temp_accuracies)])
+    x2, remainder_data, remainder_comparison = filter_correct_prediction(remainder_data, remainder_comparison, temp_accuracies)
     x2_averages = correct_prediction_sumless_mean(x2, argmax(temp_accuracies))
     averages[:, argmax(temp_accuracies)] = x2_averages
     temp_accuracies[argmax(temp_accuracies)] = 0.0
 
-    x3, remainder_data = filter_correct_prediction(remainder_data, comparison[:, argmax(temp_accuracies)])
+    x3, remainder_data, remainder_comparison = filter_correct_prediction(remainder_data, remainder_comparison, temp_accuracies)
     x3_averages = correct_prediction_sumless_mean(x3, argmax(temp_accuracies))
     averages[:, argmax(temp_accuracies)] = x3_averages
     temp_accuracies[argmax(temp_accuracies)] = 0.0
 
-    x4, remainder_data = filter_correct_prediction(remainder_data, comparison[:, argmax(temp_accuracies)])
+    x4, remainder_data, remainder_comparison = filter_correct_prediction(remainder_data, remainder_comparison, temp_accuracies)
     x4_averages = correct_prediction_sumless_mean(x4, argmax(temp_accuracies))
     averages[:, argmax(temp_accuracies)] = x4_averages
     temp_accuracies[argmax(temp_accuracies)] = 0.0
